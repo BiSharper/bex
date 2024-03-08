cfebd714496f3ef9acb8be9c4e88987bbcf1fb02
ryann <flipperplz@protonmail.com>
2024-03-08 07:36:04 -0800

    feat: Start Initial Rewrite

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,119 +1,135 @@
-use std::error::Error;
-use std::fmt::Debug;
-use std::{io, ops};
-use crate::read::Analyser;
+use std::io;
+use std::io::{Read, Seek, SeekFrom};
 
-/// Lexer struct which contains current cursor position and contents to analyze
-///
-/// # Type Parameters
-/// * `T` - Any type that is Sized (has a constant size in memory), and can be compared for equality.
-pub struct Lexer<T: Sized + PartialEq + Copy> {
-    cursor:      usize,
-    contents:    Vec<T>
+pub struct Bexer<R: Read + Seek> {
+    reader: R,
+    peeked: Option<Option<char>>
 }
 
-impl<T: Sized + PartialEq + Copy> Lexer<T> {
-    pub fn new<C: AsRef<[T]>>(content: C) -> Self {
-        Self {
-            cursor: 0,
-            contents: content.as_ref().to_vec(),
+impl<R: Read + Seek> Bexer<R> {
+    pub fn create(reader: R) -> Self { Self { reader, peeked: None } }
+
+    pub fn step_back(&mut self) -> io::Result<u64>  {
+        self.peeked = None;
+        self.reader.seek(SeekFrom::Current(-1))
+    }
+
+    pub fn step_forward(&mut self) -> io::Result<u64>  {
+        self.peeked = None;
+        self.reader.seek(SeekFrom::Current(1))
+    }
+
+    pub fn reset(&mut self) -> io::Result<u64>  {
+        self.peeked = None;
+        self.reader.seek(SeekFrom::Start(0))
+    }
+
+    pub fn take(&mut self, target: char) -> io::Result<bool> { match self.peek()? {
+        None => Ok(false),
+        Some(found) => match found {
+            _ if found == target => Ok(true),
+            _ => Ok(false)
+        }
+    }}
+
+    pub fn take_while_char(&mut self, char: char) -> io::Result<usize> {
+        self.take_while(&char, |char, found |
+            found.is_some_and(|c | c == *char)
+        )
+    }
+
+    pub fn take_until_char(&mut self, char: char) -> io::Result<usize> {
+        self.take_while(&char, |char, found |
+            found.is_some_and(|c | c != *char)
+        )
+    }
+
+    pub fn take_until_chars(&mut self, chars: &[char]) -> io::Result<usize> {
+        self.take_while(chars, |chars, found |
+            found.is_some_and(|c | !chars.contains(&c))
+        )
+    }
+
+    pub fn take_while_chars(&mut self, chars: &[char]) -> io::Result<usize> {
+        self.take_while(chars, |chars, found |
+            found.is_some_and(|c | chars.contains(&c))
+        )
+    }
+
+    pub fn take_while<T: ?Sized>(&mut self, predicate_data: &T, predicate: fn(&T, Option<char>) -> bool) -> io::Result<usize> {
+        let mut count: usize = 0;
+
+        while predicate(predicate_data, self.peek()?) {
+            self.step_forward()?;
+            count += 1;
+        }
+
+        Ok(count)
+    }
+
+    pub fn take_multi(&mut self, string: &str) -> io::Result<bool> {
+        let mut length: usize = 0;
+        for i in string.chars() { match self.take(i)? {
+            true => length += 1,
+            false => break
+        }}
+        Ok(length == string.len())
+    }
+
+    pub fn peek(&mut self) -> io::Result<Option<char>> {
+        match self.peeked {
+            None => {
+                let char = decode_char(&mut self.reader, true)?;
+                self.peeked = Some(char);
+                Ok(char)
+            }
+            Some(peeked) => Ok(peeked)
         }
     }
 
-    pub fn extract(
-        &mut self,
-        range: ops::Range<usize>
-    ) -> Vec<T> {
-        let start = range.start;
-        let end = range.end;
-        let extraction_result = self.contents.drain(range).collect::<Vec<T>>();
+    pub fn consume(&mut self) -> io::Result<Option<char>> {
+        match self.peeked {
+            None => {
+                let char = decode_char(&mut self.reader, true)?;
+                self.peeked = Some(char);
+                Ok(self.peeked.unwrap())
+            }
+            Some(peeked) => match peeked {
+                None => Ok(None),
+                Some(peeked) => {
+                    self.reader.seek(SeekFrom::Current(peeked.len_utf8() as i64))?;
+                    Ok(Some(peeked))
+                }
+            }
 
-        if start <= self.cursor && self.cursor < end {
-            self.cursor = start;
-        } else if end < self.cursor {
-            self.cursor -= end;
+        }
+    }
+}
+
+fn decode_char<R: Read + Seek>(reader: &mut R, quietly: bool) -> io::Result<Option<char>> {
+    let mut bytes = [0; 4];
+    let mut len = 0;
+
+    loop {
+        match reader.read(&mut bytes[len..len + 1]) {
+            Ok(0) => break, // End of file
+            Ok(n) => len += n,
+            Err(ref e) if e.kind() == io::ErrorKind::Interrupted => continue,
+            Err(e) => return Err(e),
         }
 
-        extraction_result
-    }
-}
-
-/// Defines methods for generating a token.
-pub trait Token<T: Sized + PartialEq + Copy> where Self: Sized {
-    type Error: From<io::Error> + Debug;
-
-    /// Generates the next token from Lexer.
-    ///
-    /// # Arguments
-    ///
-    /// * `lexer` - Lexer from which the token should be generated.
-    fn next_token(lexer: &mut Lexer<T>) -> Result<Self, Self::Error>;
-}
-
-/// Defines methods for generating a token using a specific lexical scope (can be used for lexer-hacks).
-pub trait ScopedToken<T: Sized + PartialEq + Copy> where Self: Sized {
-    type Scope: Default;
-    type Error: From<io::Error> + Debug;
-
-    /// Generates the next token from Lexer using the defined Scope.
-    ///
-    /// # Arguments
-    ///
-    /// * `lexer` - Lexer from which the token should be generated.
-    /// * `scope` - the scope for generating the token.
-    fn next_token(lexer: &mut Lexer<T>, scope: &mut Self::Scope) -> Result<Self, Self::Error>;
-}
-
-impl<T: Sized + PartialEq + Copy, Scoped: ScopedToken<T>> Token<T> for Scoped {
-    type Error = <Scoped as ScopedToken<T>>::Error;
-
-    /// Generates the next token in the default scope.
-    ///
-    /// # Arguments
-    ///
-    /// * `lexer` - Lexer from which the token should be generated.
-    fn next_token(lexer: &mut Lexer<T>) -> Result<Self, Self::Error> {
-        <Scoped as ScopedToken<T>>::next_token(lexer, &mut Scoped::Scope::default())
-    }
-}
-
-impl<T: Sized + PartialEq + Copy> Lexer<T> {
-    pub fn tokenize_until_end<
-        TokenType: Token<T>
-    >(mut self) -> Result<Vec<TokenType>, TokenType::Error> {
-        let mut tokens = vec![];
-        while !self.is_end() {
-            tokens.push(TokenType::next_token(&mut self)?)
+        if let Some(c) = std::str::from_utf8(&bytes[..len]).ok().and_then(|s| s.chars().next()) {
+            if quietly { reader.seek(SeekFrom::Current(-(len as i64)))?; }
+            return Ok(Some(c));
         }
-        Ok(tokens)
+
+        if len == 4 {
+            if quietly { reader.seek(SeekFrom::Current(-(len as i64)))?; }
+            return Err(io::Error::new(io::ErrorKind::InvalidData, "Invalid UTF-8 sequence", ));
+        }
     }
+
+    if quietly { reader.seek(SeekFrom::Current(-(len as i64)))?; }
+    return Ok(None)
 }
 
-impl<T: Sized + PartialEq + Copy> Analyser<T> for Lexer<T> {
-    /// Get the entire sequence being analyzed
-    ///
-    /// # Returns
-    /// Array slice of the sequence being analyzed
-    fn contents(&self) -> &[T] { &self.contents[..] }
-
-    /// Get the current position of cursor within the sequence
-    ///
-    /// # Returns
-    /// Cursor position as usize
-    fn pos(&self) -> usize { self.cursor }
-
-    /// Consumes the analyser, returning the sequence being analyzed
-    ///
-    /// # Returns
-    /// The sequence being analyzed as an owned vector
-    fn drain(self) -> Vec<T> { self.contents }
-
-    /// Sets the cursor to a given position
-    ///
-    /// # Parameters
-    /// * `position: usize` - The index in sequence, where cursor will be placed
-    ///
-    /// # Returns
-    /// `std::io::Result<()>` - Ok if operation successful, otherwise an Err with the `std::io::Error`
-    fn set_pos(&mut self, position: usize) -> io::Result<()> { Ok(self.cursor = position) }
-}
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,1 @@
-pub mod read; pub use read::*;
-pub mod lexer; pub use lexer::*;
-pub mod parse; pub use parse::*;
-pub mod process; pub use process::*;
+mod lexer; pub use lexer::*;
